25097a10dd503b8b6e018fed9a1d65de2e9a1177
sasatake <t06418as@gmail.com>
2022-03-24 08:00:45 +0900

    add script for creating azure ad application

---
+++ createOIDCApp.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+set -eu
+
+if !(type az && type gh > /dev/null 2>&1); then
+  echo "should install azure-cli and github-cli."
+  exit
+fi
+
+appName=terraform-provisioning-app
+repoUser=sasatake
+repoName=azure-terraform-patterns
+branchName=master
+
+subscriptionId=$(az account list --all --query '[?isDefault].id' --output tsv --only-show-errors)
+tenantId=$(az account list --all --query '[?isDefault].tenantId' --output tsv --only-show-errors)
+
+echo "create app."
+appId=$(az ad app create --display-name $appName --query appId --output tsv --only-show-errors)
+objectId=$(az ad app list --display-name $appName --query '[0].objectId' --output tsv --only-show-errors)
+echo ""
+
+echo "create service principle."
+size=$(az ad sp list --display-name $appName --query 'length([])' --only-show-errors)
+if [ $((size)) -eq 0 ]; then
+  assigneeObjectId=$(az ad sp create --id $appId --query objectId --output tsv --only-show-errors)
+else
+  assigneeObjectId=$(az ad sp list --display-name $appName --query '[0].objectId' --output tsv --only-show-errors)
+fi
+echo ""
+
+echo "create role assignment."
+az role assignment create \
+  --role contributor \
+  --subscription $subscriptionId \
+  --assignee-object-id $assigneeObjectId \
+  --assignee-principal-type ServicePrincipal \
+  --only-show-errors > /dev/null
+echo ""
+
+# objectId=a49c0939-4502-41b3-9b30-50b0ed2e217c
+uri="https://graph.microsoft.com/beta/applications/${objectId}/federatedIdentityCredentials"
+subject="repo:${repoUser}/${repoName}:ref:refs/heads/${branchName}"
+
+createFederatedIdentity(){
+  echo 'create federatedIdentityCredentials.'
+  az rest \
+    --method POST \
+    --uri ${uri} \
+    --body "{'name':'${appName}','subject':'${subject}','description':'Provisioning Terraform By GitHub Actions.','issuer':'https://token.actions.githubusercontent.com','audiences':['api://AzureADTokenExchange']}" \
+    > /dev/null
+}
+
+updateFederatedIdentity(){
+  echo 'update federatedIdentityCredentials.'
+  az rest \
+    --method PATCH \
+    --uri "${uri}/${appName}" \
+    --body "{'name':'${appName}','subject':'${subject}','description':'Provisioning Terraform By GitHub Actions.','issuer':'https://token.actions.githubusercontent.com','audiences':['api://AzureADTokenExchange']}" \
+    > /dev/null
+}
+
+set +e
+
+az rest \
+  --method GET \
+  --uri ${uri} >& /dev/null
+
+federatedIdentityIsCreated=$?
+
+if [ $((federatedIdentityIsCreated)) -eq 0 ]; then
+  updateFederatedIdentity
+else
+  createFederatedIdentity  
+fi
+echo ""
+echo ""
+
+set -e
+
+gh secret set AZURE_CLIENT_ID --body ${appId} --app actions --repos ${repoUser}/${repoName}
+gh secret set AZURE_SUBSCRIPTION_ID --body ${subscriptionId} --app actions --repos ${repoUser}/${repoName}
+gh secret set AZURE_TENANT_ID --body ${tenantId} --app actions --repos ${repoUser}/${repoName}
