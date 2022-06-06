d813bcd4017a9b5a7fcec708fc55ad917b842f27
sasatake <t06418as@gmail.com>
2022-06-07 06:56:32 +0900

    change functions os

--- modules/functions-java/functions.tf
+++ modules/functions-java/functions.tf
@@ -2,22 +2,24 @@ resource "azurerm_service_plan" "functions_java" {
   name                = "${var.prefix}-service-plan"
   resource_group_name = azurerm_resource_group.functions_java.name
   location            = var.region
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "Y1"
 
   tags = local.default_tag
 }
 
-resource "azurerm_linux_function_app" "functions_java" {
-  name                        = "${var.prefix}-functions"
-  resource_group_name         = azurerm_resource_group.functions_java.name
-  location                    = var.region
-  service_plan_id             = azurerm_service_plan.functions_java.id
-  storage_account_name        = azurerm_storage_account.functions_java.name
-  functions_extension_version = "~4"
+resource "azurerm_windows_function_app" "functions_java" {
+  location                      = var.region
+  name                          = "${var.prefix}-functions"
+  resource_group_name           = azurerm_resource_group.functions_java.name
+  service_plan_id               = azurerm_service_plan.functions_java.id
+  functions_extension_version   = "~4"
+  storage_account_name          = azurerm_storage_account.functions_java.name
+  storage_uses_managed_identity = true
 
   app_settings = {
     AppInsights_InstrumentationKey = azurerm_application_insights.functions_java.instrumentation_key
+    WEBSITE_RUN_FROM_PACKAGE       = 1
   }
 
   site_config {
